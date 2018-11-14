cb89121f7a9bef7f63632a9bda7fd689c091e9d1
Liran <liran.mor@gmail.com>
2018-11-14 10:07:58 +0200

    add AC and result + fix UI

--- simple-calculator/src/Calculator.jsx
+++ simple-calculator/src/Calculator.jsx
@@ -1,20 +1,97 @@
 import React, { Component } from 'react';
 import './Calculator.css';
-import Controls from './Controls';
+import Keys from './Keys';
 import Screen from './Screen';
+
+const performCalculation = {
+  '÷': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+  x: (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+  '=': (firstOperand, secondOperand) => secondOperand,
+};
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { expression: '', result: 0 };
+    this.state = { result: '0', firstOperand: null, waitingForSecondOperand: false, operator: null };
   }
 
-  expressionChangedHandler = event => {
-    let expression = this.state.expression;
-    let char = event.target.value;
-    let newExpression = expression + char;
+  handleOperator = nextOperator => {
+    const { firstOperand, waitingForSecondOperand, result, operator } = this.state;
+    const inputValue = parseFloat(result);
 
-    this.setState({ expression: newExpression });
+    if (operator && waitingForSecondOperand) {
+      this.setState({ operator: nextOperator });
+      return;
+    }
+
+    if (firstOperand == null) {
+      this.setState({ firstOperand: inputValue });
+    } else if (operator) {
+      const currentValue = firstOperand || 0;
+      const value = performCalculation[operator](currentValue, inputValue);
+
+      this.setState({ result: String(value) });
+      this.setState({ firstOperand: value });
+    }
+
+    this.setState({ waitingForSecondOperand: true });
+    this.setState({ operator: nextOperator });
+  };
+
+  inputDecimal = dot => {
+    if (this.state.waitingForSecondOperand === true) {
+      return;
+    }
+
+    if (!this.state.result.includes(dot)) {
+      this.setState({ result: this.state.result + dot });
+    }
+  };
+
+  inputDigit = digit => {
+    const { result, waitingForSecondOperand } = this.state;
+
+    if (waitingForSecondOperand === true) {
+      this.setState({ result: digit });
+      this.setState({ waitingForSecondOperand: false });
+    } else {
+      this.setState({ result: result === '0' ? digit : result + digit });
+    }
+  };
+
+  clearAll = () => {
+    this.setState({ result: '0', firstOperand: null, waitingForSecondOperand: null, operator: null });
+  };
+
+  clickHandler = event => {
+    if (event.target.className.includes('operator')) {
+      this.handleOperator(event.target.value);
+      return;
+    }
+
+    if (event.target.className.includes('decimalPoint')) {
+      this.inputDecimal(event.target.value);
+      return;
+    }
+
+    if (event.target.className.includes('AC')) {
+      this.clearAll();
+      return;
+    }
+
+    this.inputDigit(event.target.value);
+    // let expression = this.state.expression;
+    // let value = event.target.value;
+    // let newExpression = expression + value;
+
+    // let result = newExpression;
+    // this.setState({ expression: newExpression, result: result });
   };
 
   render() {
@@ -23,8 +100,8 @@ export default class Calculator extends Component {
         <div className="calc-screen">
           <Screen expression={this.state.expression} result={this.state.result} />
         </div>
-        <div className="calc-controls">
-          <Controls click={this.expressionChangedHandler} />
+        <div className="calc-keys">
+          <Keys click={this.clickHandler} />
         </div>
       </div>
     );
---
+++ simple-calculator/src/Keys.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Keys.css';
+
+export default function Keys(props) {
+  return (
+    <div className="grid-keys">
+      <button className="key AC" onClick={props.click} value="AC">
+        AC
+      </button>
+      <button className="key seven" onClick={props.click} value="7">
+        7
+      </button>
+      <button className="key eight" onClick={props.click} value="8">
+        8
+      </button>
+      <button className="key nine" onClick={props.click} value="9">
+        9
+      </button>
+      <button className="key operator subtraction" onClick={props.click} value="-">
+        -
+      </button>
+
+      <button className="key four" onClick={props.click} value="4">
+        4
+      </button>
+      <button className="key five" onClick={props.click} value="5">
+        5
+      </button>
+      <button className="key six" onClick={props.click} value="6">
+        6
+      </button>
+      <button className="key operator division" onClick={props.click} value="÷">
+        ÷
+      </button>
+
+      <button className="key one" onClick={props.click} value="1">
+        1
+      </button>
+      <button className="key two" onClick={props.click} value="2">
+        2
+      </button>
+      <button className="key three" onClick={props.click} value="3">
+        3
+      </button>
+      <button className="key operator multiplication" onClick={props.click} value="x">
+        x
+      </button>
+
+      <button className="key zero" onClick={props.click} value="0">
+        0
+      </button>
+      <button className="key decimalPoint" onClick={props.click} value=".">
+        .
+      </button>
+      <button className="key operator result" onClick={props.click} value="=">
+        =
+      </button>
+      <button className="key operator addition" onClick={props.click} value="+">
+        +
+      </button>
+    </div>
+  );
+}
